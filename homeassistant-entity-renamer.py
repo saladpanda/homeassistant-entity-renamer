36681a02c78d41ec6259c08945e2a9a82172b8fa
saladpanda <4498831+saladpanda@users.noreply.github.com>
2023-07-15 16:40:35 +0200

    enable search-replace

--- homeassistant-entity-renamer.py
+++ homeassistant-entity-renamer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import argparse
 import re
 import requests
@@ -7,6 +8,7 @@ import json
 import sys
 import websocket
 import config
+from tabulate import tabulate
 
 # Determine the protocol based on TLS configuration
 protocol = 'https' if config.TLS else 'http'
@@ -39,56 +41,73 @@ def list_entities(regex=None):
             entity_data = filtered_entity_data
 
         # Output the entity IDs
-        for entity_id, _ in entity_data:
-            print(entity_id)
+        return entity_data
 
     else:
         print(f'Error: {response.status_code} - {response.text}')
+        return []
 
 
-def rename_entity(old_entity_id, new_entity_id):
-    websocket_protocol = 'wss' if config.TLS else 'ws'
-    websocket_url = f'{websocket_protocol}://{config.HOST}/api/websocket'
-    ws = websocket.WebSocket()
-    ws.connect(websocket_url)
+def rename_entities(entity_data, search_regex, replace_regex):
+    renamed_data = []
+    for entity_id, friendly_name in entity_data:
+        new_entity_id = re.sub(search_regex, replace_regex, entity_id)
+        renamed_data.append((entity_id, new_entity_id, friendly_name))
 
-    auth_req = ws.recv()
+    # Print the table with original and new entity names
+    print("\nOriginal Entity Names vs New Entity Names:")
+    table = [("Original Entity Name", "New Entity Name", "Friendly Name")] + renamed_data
+    print(tabulate(table, headers="firstrow"))
 
-    # Authenticate with Home Assistant
-    auth_msg = json.dumps({"type": "auth", "access_token": config.ACCESS_TOKEN})
-    ws.send(auth_msg)
-    auth_result = ws.recv()
-    auth_result = json.loads(auth_result)
-    if auth_result["type"] != "auth_ok":
-        print("Authentication failed. Check your access token.")
-        return
+    # Ask user for confirmation
+    answer = input("\nDo you want to proceed with renaming the entities? (y/N): ")
+    if answer.lower() == "y" or answer.lower() == "yes":
+        websocket_protocol = 'wss' if config.TLS else 'ws'
+        websocket_url = f'{websocket_protocol}://{config.HOST}/api/websocket'
+        ws = websocket.WebSocket()
+        ws.connect(websocket_url)
 
-    # Update the entity registry
-    entity_registry_update_msg = json.dumps({
-        "id": 1,
-        "type": "config/entity_registry/update",
-        "entity_id": old_entity_id,
-        "new_entity_id": new_entity_id
-    })
-    ws.send(entity_registry_update_msg)
-    update_result = ws.recv()
-    update_result = json.loads(update_result)
-    if update_result["success"]:
-        print("Entity registry updated successfully!")
+        auth_req = ws.recv()
+
+        # Authenticate with Home Assistant
+        auth_msg = json.dumps({"type": "auth", "access_token": config.ACCESS_TOKEN})
+        ws.send(auth_msg)
+        auth_result = ws.recv()
+        auth_result = json.loads(auth_result)
+        if auth_result["type"] != "auth_ok":
+            print("Authentication failed. Check your access token.")
+            return
+
+        # Rename the entities
+        for index, (entity_id, new_entity_id, _) in enumerate(renamed_data):
+            entity_registry_update_msg = json.dumps({
+                "id": index+1,
+                "type": "config/entity_registry/update",
+                "entity_id": entity_id,
+                "new_entity_id": new_entity_id
+            })
+            ws.send(entity_registry_update_msg)
+            update_result = ws.recv()
+            update_result = json.loads(update_result)
+            if update_result["success"]:
+                print(f"Entity '{entity_id}' renamed to '{new_entity_id}' successfully!")
+            else:
+                print(f"Failed to rename entity '{entity_id}': {update_result['error']['message']}")
+
+        ws.close()
     else:
-        print("Failed to update entity registry:", update_result["error"]["message"])
-
-    ws.close()
+        print("Renaming process aborted.")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="HomeAssistant Entity Renamer")
-    parser.add_argument('regex', nargs='?', help='Filter the output using a regular expression')
-    parser.add_argument('--old', help='Old entity ID')
-    parser.add_argument('--new', help='New entity ID')
+    parser.add_argument('search_regex', help='Regular expression for search')
+    parser.add_argument('replace_regex', help='Regular expression for replace')
     args = parser.parse_args()
 
-    if args.old and args.new:
-        rename_entity(args.old, args.new)
+    entity_data = list_entities(args.search_regex)
+
+    if entity_data:
+        rename_entities(entity_data, args.search_regex, args.replace_regex)
     else:
-        list_entities(args.regex)
+        print("No entities found matching the search regex.")
